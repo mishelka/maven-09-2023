096d410ddf27832350da8839e030ca9c8da3470e
Michaela Bačíková <bacikova.michaela@gmail.com>
2023-09-21 09:37:18 +0200

    exec plugin example

--- maven-java-0923/src/main/java/sk/cde/training/HelloWorld.java
+++ maven-java-0923/src/main/java/sk/cde/training/HelloWorld.java
@@ -1,5 +1,10 @@
+package sk.cde.training;
+
 public class HelloWorld {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello World!!!!");
+        for (int a = 0; a < args.length; a++) {
+            System.out.println(args[a]);
+        }
     }
 }
